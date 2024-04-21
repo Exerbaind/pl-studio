f38202b8d58b2aa73303673c4834fcb9c1e1cd8c
Дмитрий Шамаль <shamaldi@pik.ru>
2024-04-21 15:21:33 +0300

    feat: slider

--- script.js
+++ script.js
@@ -2,3 +2,33 @@ window.onload = function() {
     const page = document.querySelector('body');
     page.classList.remove('body-hider');
 };
+
+const sliderWrpper = document.getElementById('students-slider-wrapper');
+const sliderItems = [...document.querySelectorAll('.students-slider-item')];
+const sliderBack = document.querySelector('.students-slider-back-btn');
+const sliderNext = document.querySelector('.students-slider-next-btn');
+
+let sliderTransformValue = 0;
+
+function handleNext () {
+    if (sliderTransformValue > -((sliderItems.length - 1) * 100)) {
+        sliderTransformValue = sliderTransformValue - 100;
+        sliderWrpper.style.transform = `translateX(${sliderTransformValue}%)`;
+    } else {
+        sliderTransformValue = 0;
+        sliderWrpper.style.transform = `translateX(${sliderTransformValue}%)`;
+    }
+}
+
+function handleBack () {
+    if (sliderTransformValue < 0) {
+        sliderTransformValue = sliderTransformValue + 100;
+        sliderWrpper.style.transform = `translateX(${sliderTransformValue}%)`;
+    } else {
+        sliderTransformValue = (sliderItems.length - 1) * 100 * -1;
+        sliderWrpper.style.transform = `translateX(${sliderTransformValue}%)`;
+    }
+}
+
+sliderBack.addEventListener('click', handleBack);
+sliderNext.addEventListener('click', handleNext);
